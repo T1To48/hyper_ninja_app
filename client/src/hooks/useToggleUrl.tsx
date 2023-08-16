b1134efe89c031cfddf72dc858c36ec90ba8874e
T1To48 <tofek.98@gmail.com>
2023-08-16 16:03:02 +0300

    #

--- client/src/hooks/useToggleUrl.tsx
+++ client/src/hooks/useToggleUrl.tsx
@@ -1,31 +1,28 @@
-import {  useReviveUrlByIdMutation, useUpdateUrlByIdMutation } from '../features/api.slice'
-import {Status} from "../Types/url.types"
-import { useReviveUrl } from './useReviveUrl';
-const useToggleUrl = ({status,id}:{status:Status,id:string}) => {
-  const [reviveUrl]=useReviveUrl()
-  const [updateUrlById]=useUpdateUrlByIdMutation();
-  const [reviveUrlById]=useReviveUrlByIdMutation()
-  const toggleUrl=async ():Promise<void>=>{
-    const newStatus:Status=status==="Off"?"Loading":"Off"
-    const updateObj={
-      id,data:{status:newStatus}
+import { useUpdateUrlByIdMutation } from "../features/api.slice";
+import { Status } from "../Types/url.types";
+import { useReviveUrl } from "./useReviveUrl";
+const useToggleUrl = ({ status, id }: { status: Status; id: string }) => {
+  const [reviveUrl] = useReviveUrl();
+  const [updateUrlById] = useUpdateUrlByIdMutation();
+  const toggleUrl = async (): Promise<void> => {
+    const newStatus: Status = status === "Off" ? "Loading" : "Off";
+    const updateObj = {
+      id,
+      data: { status: newStatus },
+    };
+    try {
+      const updatedUrl = await updateUrlById(updateObj).unwrap();
+      if (updatedUrl.success && updatedUrl.data.status !== "Off") {
+        void reviveUrl(id);
+      } else if (updatedUrl.success && updatedUrl.data.status === "Off") return;
+      else {
+        throw new Error(JSON.stringify(updatedUrl));
+      }
+    } catch (err) {
+      console.log("error in turning on/off the url, Error==>  ", err);
     }
-    try{
-     const updatedUrl= await updateUrlById(updateObj).unwrap();
-     if(updatedUrl.success &&updatedUrl.data.status!=="Off"){
-       
-      //  void reviveUrlById(id)
-      void reviveUrl(id)
-     }else if(updatedUrl.success &&updatedUrl.data.status==="Off")return;
-  else{
-    throw new Error(JSON.stringify(updatedUrl))
-  }
-    }catch(err){
-      console.log("error in turning on/off the url, Error==>  ",err)
-    }
-      
-  }
-    return [toggleUrl] as const
-}
+  };
+  return [toggleUrl] as const;
+};
 
-export default useToggleUrl
+export default useToggleUrl;
