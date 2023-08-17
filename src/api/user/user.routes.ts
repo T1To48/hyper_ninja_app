825bcd62c7a6bdac1c12e74139d64bc575ca5a13
T1To48 <tofek.98@gmail.com>
2023-08-17 22:20:10 +0300

    deleted the comment out

--- src/api/user/user.routes.ts
+++ src/api/user/user.routes.ts
@@ -9,30 +9,19 @@ import {
   updateUser,
   deleteUser,
 } from "./user.controllers";
-import {
-   changePassword,
-    checkTokenCookie } from "./user.services";
+import { changePassword, checkTokenCookie } from "./user.services";
 
 const router: Router = Router();
 router.post("/register", register);
 router.post("/login", login);
-router.post("/logout", protect,logout);
-router.post("/check-token-cookie",protect,checkTokenCookie)
+router.post("/logout", protect, logout);
+router.post("/check-token-cookie", protect, checkTokenCookie);
 router
-.use(protect)
-.route("/")
-.get(getUser)
-.patch(updateUser)
-.delete(checkPassword, deleteUser);
-router.patch("/change-password",protect,checkPassword,changePassword)
-
-//login as guest with fixed data
-// restore delete all urls and then send to frontend
-// 
-
-//LAST LOGIN
-//LOGOUT
-//TOKEN & cookie RENEW
-
+  .use(protect)
+  .route("/")
+  .get(getUser)
+  .patch(updateUser)
+  .delete(checkPassword, deleteUser);
+router.patch("/change-password", protect, checkPassword, changePassword);
 
 export default router;
